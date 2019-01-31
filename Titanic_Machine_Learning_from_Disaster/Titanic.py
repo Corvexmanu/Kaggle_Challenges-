d50e6d9adad9f1a55a0110e729b0ac239ebddbf0
Victor Manuel Villamil Romero <n10080236@qut.edu.au>
2019-02-01 09:10:22 +1000

    Titanic V0.01

--- Titanic_Machine_Learning_from_Disaster/Titanic.py
+++ Titanic_Machine_Learning_from_Disaster/Titanic.py
@@ -1,2 +1,9 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-import
+
+#Import the datasets.
+train_data = pd.read_csv(r"C:\Users\nex1axi\OneDrive - Queensland University of Technology\MIT\GitHub_Projects\Kaggle_Challenges\Titanic_Machine_Learning_from_Disaster\Resources\train.csv")
+test_data = pd.read_csv(r"C:\Users\nex1axi\OneDrive - Queensland University of Technology\MIT\GitHub_Projects\Kaggle_Challenges\Titanic_Machine_Learning_from_Disaster\Resources\test.csv")
+
+
